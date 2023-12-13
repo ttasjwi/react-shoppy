6788ecca871efcebade00c6d26541e64ff266faa
ttasjwi <88282356+ttasjwi@users.noreply.github.com>
2023-12-13 22:13:32 +0900

    라우팅 헤더 구현 (#1)
    * routing header - 마크업
    
    * routing header - 스타일링

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {FiShoppingBag} from 'react-icons/fi';
+import {BsFillPencilFill} from 'react-icons/bs';
+
+const Navbar = () => {
+
+    return (
+        <header className='flex justify-between border-b border-gray-300 p-2'>
+            <Link to='/' className='flex items-center text-4xl text-brand'>
+                <FiShoppingBag/>
+                <h1>Shoppy</h1>
+            </Link>
+            <nav className='flex items-center gap-4 font-semibold'>
+                <Link to='/products'>Products</Link>
+                <Link to='/my-cart'>MyCart</Link>
+                <Link to='/products/register' className='text-2xl'>
+                    <BsFillPencilFill />
+                </Link>
+                <button>Login</button>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+
+    return (
+        <>
+            페이지를 찾지 못 했습니다.
+        </>
+    );
+}
+
+export default NotFound;
