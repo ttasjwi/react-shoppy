3a9f646d0185f1aa12771ae47848e97cae9cdeff
ttasjwi <ttasjwi920@gmail.com>
2023-12-14 11:52:30 +0900

    login - 로그인/로그아웃 버튼 클릭 시 로그인/로그아웃

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import {FiShoppingBag} from 'react-icons/fi';
 import {BsFillPencilFill} from 'react-icons/bs';
+import {login, logout} from "../api/firebase";
 
 const Navbar = () => {
+    const [user, setUser] = useState(null);
+    const handleLogin = () => {
+        login().then(user => setUser(user));
+    };
+
+    const handleLogout = () => {
+        logout().then(setUser);
+    };
 
     return (
         <header className='flex justify-between border-b border-gray-300 p-2'>
@@ -17,7 +26,10 @@ const Navbar = () => {
                 <Link to='/products/register' className='text-2xl'>
                     <BsFillPencilFill />
                 </Link>
-                <button>Login</button>
+                {user
+                    ? <button type='button' onClick={handleLogout}>Logout</button>
+                    : <button type='button' onClick={handleLogin}>Login</button>
+                }
             </nav>
         </header>
     );
