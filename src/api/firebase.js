b6270f5d38e91b92ac2e4ad7d7f11203fb6591cf
ttasjwi <ttasjwi920@gmail.com>
2023-12-16 10:42:03 +0900

    admin ui - 사용자 admin 여부를 user 객체에 추가

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -2,6 +2,8 @@
 import {initializeApp} from "firebase/app";
 
 import {getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut} from "firebase/auth";
+import {get, getDatabase, ref} from "firebase/database";
+
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -13,6 +15,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
+const database = getDatabase(app);
 
 const provider = new GoogleAuthProvider();
 
@@ -25,10 +28,30 @@ export const login = () => signInWithPopup(auth, provider)
 
 export const logout = () => signOut(auth);
 
+/**
+ * 사용자에게 Admin 권한이 있는 지 확인 후, 어드민 플래그를 삽입
+ * @param user
+ * @returns {Promise<void>}
+ */
+const addIsAdminFlag = async (user) => {
+    return get(ref(database, 'admins'))
+        .then(snapShot => {
+            if (snapShot.exists()) {
+                const admins = snapShot.val();
+                const isAdmin = admins.includes(user.uid);
+                return {...user, isAdmin};
+            }
+            return user;
+        })
+};
+
+
 // 전역 인증 객체에 연결해서, 사용자 정보를 받아오고 callback에 user를 전달해서 실행
 export const onUserStateChange = (callback) => {
     // 인증 상태가 변경될 때마다 전달받은 콜백을 실행하도록 함
-    onAuthStateChanged(auth, (user) => {
-        callback(user);
+    onAuthStateChanged(auth, async (user) => {
+        // 사용자가 admin인지 여부를 판단하여, 사용자에 isAdminFlag를 추가함
+        const updatedUser = user ? await addIsAdminFlag(user) : null;
+        callback(updatedUser);
     })
 };
