c7a751cbebdda9c06f2d3918150536f1b44dfd63
ttasjwi <ttasjwi920@gmail.com>
2023-12-21 17:27:51 +0900

    cart - 카트 페이지 스타일링

--- src/components/PriceCard.jsx
+++ src/components/PriceCard.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const PriceCard = ({text, price}) => {
     return (
-        <>
-        </>
+        <div className='bg-gray-50 p-8 mx-2 rounded-2xl text-center text-lg md:tex-xl'>
+            <p>{text}</p>
+            <p className='font-bold text-brand text-xl md:text-2xl'>₩{price}</p>
+        </div>
     );
 }
 
--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -6,12 +6,13 @@ import CartItem from "../components/CartItem";
 import PriceCard from "../components/PriceCard";
 import {BsFillPlusCircleFill} from 'react-icons/bs';
 import {FaEquals} from 'react-icons/fa';
+import Button from "../components/ui/Button";
 
 const SHIPPING_PRICE = 3000;
 
 const MyCart = () => {
     const {userId} = useAuthContext();
-    const {isLoading, error, data:products} = useQuery({
+    const {isLoading, error, data: products} = useQuery({
         queryKey: ['carts'],
         queryFn: () => getCart(userId)
     });
@@ -24,21 +25,24 @@ const MyCart = () => {
     const totalPrice = products && products.reduce((prev, current) => prev + parseInt(current.price) * current.quantity, 0)
 
     return (
-        <section>
-            <p>내 장바구니</p>
+        <section className='p-8 flex flex-col'>
+            <p className='text-2xl text-center font-bold pb-4 border-b border-gray-300'>내 장바구니</p>
             {!hasProducts && <p>장바구니에 상품이 없습니다.</p>}
-            {hasProducts && <>
-                <ul>
-                    {products && products.map(product => <CartItem key={product.id} product={product} userId={userId} />)}
-                </ul>
-            </>}
-            <div>
-                <PriceCard text="상품 총액" price={totalPrice} />
-                <BsFillPlusCircleFill />
-                <PriceCard text="배송액" price={SHIPPING_PRICE} />
-                <FaEquals />
-                <PriceCard text="총가격" price={totalPrice + SHIPPING_PRICE} />
-            </div>
+            {hasProducts && (
+                <>
+                    <ul className='border-b border-gray-300 mb-8 p-4 px-8'>
+                        {products && products.map(product => <CartItem key={product.id} product={product}
+                                                                       userId={userId}/>)}
+                    </ul>
+                    <div className='flex justify-between items-center mb-6 p-2 md:px-8 lg:px-16'>
+                        <PriceCard text="상품 총액" price={totalPrice}/>
+                        <BsFillPlusCircleFill className='shrink-0'/>
+                        <PriceCard text="배송액" price={SHIPPING_PRICE}/>
+                        <FaEquals className='shrink-0'/>
+                        <PriceCard text="총가격" price={totalPrice + SHIPPING_PRICE}/>
+                    </div>
+                    <Button text='주문하기' />
+                </>)}
         </section>
     );
 }
